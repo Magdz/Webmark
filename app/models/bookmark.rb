90eab399eedb9b93e260c54d2efba32bf0966d23
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2015-08-22 13:21:52 +0200

    paperclip image fixed

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,5 @@
 class Bookmark < ActiveRecord::Base
+	Paperclip.options[:command_path] = 'C:/Program Files/ImageMagick-6.9.2-Q16'
 	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
