ab1b28762255fe212e851574da4ddc536196426f
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2015-08-22 14:14:35 +0200

    Add association between Bookmark and User

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,4 +2,6 @@ class Bookmark < ActiveRecord::Base
 	Paperclip.options[:command_path] = 'C:/Program Files/ImageMagick-6.9.2-Q16'
 	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
+	belongs_to :user
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150822102542) do
+ActiveRecord::Schema.define(version: 20150822121120) do
 
   create_table "bookmarks", force: true do |t|
     t.string   "title"
@@ -23,7 +23,10 @@ ActiveRecord::Schema.define(version: 20150822102542) do
     t.string   "image_content_type"
     t.integer  "image_file_size"
     t.datetime "image_updated_at"
+    t.integer  "user_id"
   end
+
+  add_index "bookmarks", ["user_id"], name: "index_bookmarks_on_user_id"
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
