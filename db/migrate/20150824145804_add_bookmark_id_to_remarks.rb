527c7f9a84445b7a6f66fb0ee48b5f6ecc834486
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2015-08-24 17:04:15 +0200

    Add Association between Bookmark and Remark

---
+++ db/migrate/20150824145804_add_bookmark_id_to_remarks.rb
@@ -0,0 +1,4 @@
+class AddBookmarkIdToRemarks < ActiveRecord::Migration
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150824093413) do
+ActiveRecord::Schema.define(version: 20150824145804) do
 
   create_table "bookmarks", force: true do |t|
     t.string   "title"
