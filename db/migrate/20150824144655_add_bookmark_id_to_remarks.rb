8c1fcbf9c17170484d52861b2e7c991d8deea50d
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2015-08-25 23:17:07 +0200

    Fix Database bug

--- db/migrate/20150824144655_add_bookmark_id_to_remarks.rb
+++
@@ -1,6 +0,0 @@
-class AddBookmarkIdToRemarks < ActiveRecord::Migration
-  def change
-    add_column :remarks, :bookmark_id, :integer
-    add_index :remarks, :bookmark_id
-  end
-end
--- db/migrate/20150824145804_add_bookmark_id_to_remarks.rb
+++
@@ -1,4 +0,0 @@
-class AddBookmarkIdToRemarks < ActiveRecord::Migration
-  def change
-  end
-end
