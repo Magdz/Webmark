e693204e6926a7385bf3865fe7b9eea7bd52bd71
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2015-08-29 02:03:00 +0200

    Add Validations

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,6 +2,7 @@ class Bookmark < ActiveRecord::Base
 	Paperclip.options[:command_path] = 'C:/Program Files/ImageMagick-6.9.2-Q16'
 	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+	validates :category_id, :subcategory_id, :title, :url, :description, presence: true
 
 	acts_as_votable
 	belongs_to :user
