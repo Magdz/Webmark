d24da51ef8ae7095cfb4eb11b915561dcba66222
Hammam94 <gentle_engieer@hotmail.com>
2015-08-29 20:40:17 +0200

    db followers

--- db/migrate/20150829112913_create_followers.rb
+++ db/migrate/20150829112913_create_followers.rb
@@ -9,4 +9,14 @@ class CreateFollowers < ActiveRecord::Migration
     add_index :followers, :follower_id
     add_index :followers, :followed_id
   end
+  def change
+    create_table :followings do |t|
+      t.integer :user_id
+      t.integer :followed_id
+
+      t.timestamps
+    end
+    add_index :followings, :followed_id
+    add_index :followings, :user_id
+  end
 end
