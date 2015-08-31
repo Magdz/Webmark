cc26e8786f61cc1d22b25d544228d470e8ba3cc7
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2015-08-31 15:22:00 +0200

    Add Avatar to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+	Paperclip.options[:command_path] = 'C:/Program Files/ImageMagick-6.9.2-Q16'
+	has_attached_file :avatar, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
+	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
