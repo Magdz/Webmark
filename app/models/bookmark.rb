ab8a3fa92123f2e660cc8aefe234952a9960c54b
Ahmed Magdy Ibrahim <magdz_008@yahoo.com>
2015-09-01 01:30:13 +0200

    Add url access

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -3,7 +3,8 @@ class Bookmark < ActiveRecord::Base
 	has_attached_file :image, styles: { large: "600x600>", medium: "300x300>", thumb: "150x150#" }
 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 	validates :category_id, :subcategory_id, :title, :url, :description, presence: true
-	
+	#validates :url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?$/ix
+
 	acts_as_votable
 	belongs_to :user
 	belongs_to :subcategory
